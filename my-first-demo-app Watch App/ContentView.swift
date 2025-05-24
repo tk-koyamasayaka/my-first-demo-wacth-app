77c7db4bb7bf4a2e3567ac555014c07782ac8cd6
tk-koyamasayaka <38.skrisyk@gmail.com>
2025-05-25 00:02:42 +0900

    demo

--- my-first-demo-app Watch App/ContentView.swift
+++ my-first-demo-app Watch App/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 8) {
+            Image(systemName: "applewatch")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 40, height: 40)
+                .foregroundStyle(.green)
+            
+            Text("こんにちは、Apple Watch！")
+                .font(.headline)
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
